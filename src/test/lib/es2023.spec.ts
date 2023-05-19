86c9af86e8c69df002a432dd181c5a2f834cd849
Carl Ansley <carl.ansley@gmail.com>
2023-05-19 15:50:29 -0400

    Le/Rama (Lema?) fixes

--- src/test/lib/es2023.spec.ts
+++ src/test/lib/es2023.spec.ts
@@ -5,7 +5,7 @@ import { strict as assert } from 'node:assert';
 import { describe, it } from '../../describe-it.test';
 
 describe('supports es2023', () => {
-  it('supports String.findLastIndex (from es2023.array)', async () => {
+  it('supports Array.findLastIndex (from es2023.array)', async () => {
     assert.equal(
       ['a', 'b', 'c'].findLastIndex((value) => value === 'b'),
       1
