0d6673763f92d3f83988f997969ed90cd2d2fcad
Carl Ansley <carl.ansley@gmail.com>
2023-08-01 11:12:56 -0400

    added tests for typescript 5.1/5.2

---
+++ src/test/typescript/typescript-5.1.spec.ts
@@ -0,0 +1,18 @@
+// typescript-5.1.ts
+
+import { strict as assert } from 'node:assert';
+
+import { describe, it } from '../../describe-it.test';
+
+describe('typescript-5.1', () => {
+  it('has easier implicit returns', () => {
+    // pre 5.1, this would be a "not all code paths return a value" error
+    function test(): undefined {
+      if (Math.random()) {
+        // eslint-disable-next-line no-useless-return
+        return;
+      }
+    }
+    assert.ok(test() === undefined);
+  });
+});
