7f9f5f834d2f17eb7ca32a4fe146aa442651588e
Carl Ansley <carl.ansley@gmail.com>
2023-11-21 13:06:51 -0500

    import attributes support

--- src/test/typescript/typescript-5.3.spec.ts
+++ src/test/typescript/typescript-5.3.spec.ts
@@ -2,21 +2,18 @@
 
 import { strict as assert } from 'node:assert';
 
-/*
- * Note: prettier currently does not support import attributes
- * import packageJson from '../../../package.json' with { type: 'json' };
- */
+// This import works, but prettier does not support import assertions yet
+
+// import packageJson from '../../../package.json' with { type: 'json' };
 
 import { describe, it } from '../../describe-it.test';
 
 describe('typescript-5.3', () => {
-  it.skip('supports import attributes', async () => {
-    /*
-     * Note: esbuild currently does not support import attributes
-     * const dynamicPackageJson = await import('../../../package.json', { with: { type: 'json' } });
-     * assert.deepEqual(dynamicPackageJson.default, packageJson);
-     * assert.equal(dynamicPackageJson.name, '@checkdigit/typescript-config');
-     */
+  it('supports import attributes', async () => {
+    const dynamicPackageJson = await import('../../../package.json', { with: { type: 'json' } });
+    // assert.deepEqual(dynamicPackageJson.default, packageJson);
+    assert.equal(dynamicPackageJson.default.description, 'Check Digit standard Typescript configuration');
+    assert.equal(dynamicPackageJson.default.name, '@checkdigit/typescript-config');
   });
 
   it('supports switch (true) narrowing', () => {
