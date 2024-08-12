c62f69371e57a8b2197a1b4fc2c5dabe0d1cd255
Carl Ansley <carl.ansley@gmail.com>
2024-08-12 15:23:15 -0400

    remove commonjs test

--- src/test/index.ts
+++ src/test/index.ts
@@ -3,7 +3,6 @@
 import './lib/es2023.spec';
 import './lib/es2024.spec';
 
-import './commonjs.spec';
 import './module.spec';
 
 import './typescript/typescript-4.4.spec';
@@ -18,3 +17,4 @@ import './typescript/typescript-5.2.spec';
 import './typescript/typescript-5.3.spec';
 import './typescript/typescript-5.4.spec';
 import './typescript/typescript-5.5.spec';
+import './typescript/typescript-5.6.spec';
