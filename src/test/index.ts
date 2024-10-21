0be435b47e0a48ce12a7c88c2ca49947113c7a67
Carl Ansley <carl.ansley@gmail.com>
2024-10-21 17:26:15 -0400

    clean up test directory

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,21 +1,4 @@
 // test/index.ts
 
-import './lib/es2023.spec';
-import './lib/es2024.spec';
-
-import './module.spec';
-
-import './typescript/typescript-4.4.spec';
-import './typescript/typescript-4.5.spec';
-import './typescript/typescript-4.6.spec';
-import './typescript/typescript-4.7.spec';
-import './typescript/typescript-4.8.spec';
-import './typescript/typescript-4.9.spec';
-import './typescript/typescript-5.0.spec';
-import './typescript/typescript-5.1.spec';
-import './typescript/typescript-5.2.spec';
-import './typescript/typescript-5.3.spec';
-import './typescript/typescript-5.4.spec';
-import './typescript/typescript-5.5.spec';
-import './typescript/typescript-5.6.spec';
-import './typescript/typescript-5.7.spec';
+import './lib';
+import './typescript';
