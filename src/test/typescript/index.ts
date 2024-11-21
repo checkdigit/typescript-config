fde3c9a87e250aa4be4b2af2e1fa6dae75e4ab94
Carl Ansley <carl.ansley@gmail.com>
2024-11-21 15:15:02 -0500

    remove pre-5.0 Typescript tests

--- src/test/typescript/index.ts
+++ src/test/typescript/index.ts
@@ -1,11 +1,5 @@
 // test/typescript/index.ts
 
-import './typescript-4.4.spec.ts';
-import './typescript-4.5.spec.ts';
-import './typescript-4.6.spec.ts';
-import './typescript-4.7.spec.ts';
-import './typescript-4.8.spec.ts';
-import './typescript-4.9.spec.ts';
 import './typescript-5.0.spec.ts';
 import './typescript-5.1.spec.ts';
 import './typescript-5.2.spec.ts';
