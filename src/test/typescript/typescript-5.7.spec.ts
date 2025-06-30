b4a0b4e4dfa436fe3f71f5b684aa63f1408e98bb
Carl Ansley <carl.ansley@gmail.com>
2025-06-30 13:27:44 -0400

    update deps

--- src/test/typescript/typescript-5.7.spec.ts
+++ src/test/typescript/typescript-5.7.spec.ts
@@ -5,6 +5,7 @@ import { describe, it } from 'node:test';
 
 describe('typescript-5.7', () => {
   it('checks for never-initialized variables', () => {
+    // eslint-disable-next-line no-unassigned-vars
     let result: number;
     function foo() {
       // @ts-expect-error Variable 'result' is used before being assigned.
