6ae0aa7c23d70332c6ed918962d98d29bdbb95f3
Carl Ansley <carl.ansley@gmail.com>
2025-08-14 13:52:50 -0400

    Support TypeScript 7, require Node 24

--- src/test/lib/es2026.spec.ts
+++ src/test/lib/es2026.spec.ts
@@ -6,6 +6,7 @@ import { describe, it } from 'node:test';
 await (process.version < 'v24' ? describe.skip : describe)('es2026', () => {
   // https://github.com/tc39/proposal-is-error
   it('supports Error.isError', async () => {
+    // @ts-expect-error an error in TypeScript 5.8/7, but valid in 5.9
     assert.ok(typeof Error.isError === 'function');
   });
 });
