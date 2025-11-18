b559d426412aa5931ff54e20014e4284b9dde1b8
Carl Ansley <carl.ansley@gmail.com>
2025-11-18 15:36:59 -0500

    add new node 24 tests

--- src/test/node/node-24.spec.ts
+++ src/test/node/node-24.spec.ts
@@ -1,7 +1,9 @@
 // test/node/node-24.spec.ts
 
 import { strict as assert } from 'node:assert';
+import crypto from 'node:crypto';
 import { describe, it } from 'node:test';
+import tls from 'node:tls';
 
 describe('node-24', () => {
   it('URLPattern exists as a global', async () => {
@@ -11,5 +13,18 @@ describe('node-24', () => {
   // 24.2+
   it('import.meta.main is available', async () => {
     assert.ok(typeof import.meta.main === 'boolean');
+  });
+
+  // 24.5+
+  it('Built-in proxy support in request() and Agent', async () => {
+    tls.setDefaultCACertificates(
+      tls.getCACertificates('default').concat(tls.getCACertificates('system')),
+    );
+  });
+
+  // 24.7+
+  it('Post-Quantum Cryptography in node:crypto', async () => {
+    assert.equal(typeof crypto.encapsulate, 'function');
+    assert.equal(typeof crypto.decapsulate, 'function');
   });
 });
