152b0afe171a8f25a08c65868f1eb7c197d01658
Carl Ansley <carl.ansley@gmail.com>
2026-05-05 19:38:37 -0400

    TypeScript 6.0 (#90)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2025 Check Digit, LLC
+ * Copyright (c) 2021-2026 Check Digit, LLC
  *
  * This code is licensed under the MIT license (see LICENSE.txt for details).
  */
@@ -12,6 +12,7 @@ import prettier from 'eslint-config-prettier';
 import json from '@eslint/json';
 import markdown from '@eslint/markdown';
 import yaml from 'eslint-plugin-yml';
+import importPlugin from 'eslint-plugin-import';
 
 const ignores = [
   ...(await fs.readFile('.gitignore', 'utf-8'))
@@ -25,6 +26,7 @@ const tsConfigurations = [
   ...ts.configs.strictTypeChecked,
   ...ts.configs.stylisticTypeChecked,
   prettier,
+  importPlugin.flatConfigs.typescript,
   {
     languageOptions: {
       parser: ts.parser,
--- src/test/lib/es2026.spec.ts
+++ src/test/lib/es2026.spec.ts
@@ -4,6 +4,12 @@ import { strict as assert } from 'node:assert';
 import { describe, it } from 'node:test';
 
 describe('es2026', () => {
+  // https://github.com/tc39/proposal-array-from-async
+  it('supports Array.fromAsync', async () => {
+    const result = await Array.fromAsync([1, 2, 3]);
+    assert.deepEqual(result, [1, 2, 3]);
+  });
+
   // https://github.com/tc39/proposal-is-error
   it('supports Error.isError', async () => {
     assert.ok(typeof Error.isError === 'function');
@@ -12,8 +18,7 @@ describe('es2026', () => {
   // https://github.com/tc39/proposal-math-sum
   it('does not support Math.sumPrecise yet', async () => {
     assert.throws(
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
+      // @ts-expect-error not supported by TypeScript 6.0
       () => assert.equal(Math.sumPrecise([1e20, 0.1, -1e20]), 0.1),
       {
         name: 'TypeError',
@@ -29,56 +34,123 @@ describe('es2026', () => {
       const array = new Uint8Array([
         72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100,
       ]);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
       assert.equal(array.toBase64, undefined);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
       assert.equal(array.toHex, undefined);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      assert.equal(array.fromBase64, undefined);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      assert.equal(array.fromHex, undefined);
+      assert.equal(Uint8Array.fromBase64, undefined);
+      assert.equal(Uint8Array.fromHex, undefined);
     });
   } else {
-    // Node 25+
+    // Node 26+
     it('supports Uint8Array to/from base64 and hex', async () => {
       const array = new Uint8Array([
         72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100,
       ]);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
       assert.deepEqual(array.toBase64(), 'SGVsbG8gV29ybGQ=');
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
       assert.deepEqual(array.toHex(), '48656c6c6f20576f726c64');
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
       assert.deepEqual(Uint8Array.fromBase64('SGVsbG8gV29ybGQ='), array);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
       assert.deepEqual(Uint8Array.fromHex('48656c6c6f20576f726c64'), array);
     });
   }
 
   // https://github.com/tc39/proposal-iterator-sequencing
-  it('does not support Iterator Sequencing yet', async () => {
-    const lows = Iterator.from([0, 1, 2, 3]);
-    const highs = Iterator.from([6, 7, 8, 9]);
-    assert.throws(
-      () =>
-        assert.deepEqual(
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-expect-error
-          Iterator.concat(lows, [4, 5], highs),
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-        ),
-      {
-        name: 'TypeError',
-        message: 'Iterator.concat is not a function',
-      },
+  if (process.version < 'v26') {
+    // Node 24
+    it('does not support Iterator Sequencing yet', async () => {
+      const lows = Iterator.from([0, 1, 2, 3]);
+      const highs = Iterator.from([6, 7, 8, 9]);
+      assert.throws(
+        () =>
+          assert.deepEqual(
+            // @ts-expect-error not supported by TypeScript 6.0
+            [...Iterator.concat(lows, [4, 5], highs)],
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          ),
+        {
+          name: 'TypeError',
+          message:
+            'Iterator.concat is not a function or its return value is not iterable',
+        },
+      );
+    });
+  } else {
+    // Node 26+
+    it('supports Iterator Sequencing', async () => {
+      const lows = Iterator.from([0, 1, 2, 3]);
+      const highs = Iterator.from([6, 7, 8, 9]);
+      assert.deepEqual(
+        // @ts-expect-error not supported by TypeScript 6.0
+        [...Iterator.concat(lows, [4, 5], highs)],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      );
+    });
+  }
+
+  // https://github.com/tc39/proposal-json-parse-with-source
+  it('supports JSON.parse source text access', async () => {
+    const tooBigForNumber = BigInt(Number.MAX_SAFE_INTEGER) + 2n;
+    assert.equal(
+      // @ts-expect-error not supported by TypeScript 6.0
+      JSON.parse(String(tooBigForNumber), (key, value, { source }) =>
+        /^[0-9]+$/u.test(source) ? BigInt(source) : value,
+      ) === tooBigForNumber,
+      true,
     );
+    // @ts-expect-error not supported by TypeScript 6.0
+    const embedded = JSON.stringify({ tooBigForNumber }, (key, val) =>
+      // @ts-expect-error not supported by TypeScript 6.0
+      typeof val === 'bigint' ? JSON.rawJSON(String(val)) : val,
+    );
+    assert.equal(embedded, '{"tooBigForNumber":9007199254740993}');
   });
+
+  // https://github.com/tc39/proposal-upsert
+  if (process.version < 'v26') {
+    // Node 24
+    it('does not support upsert yet', async () => {
+      const map = new Map<string, number>();
+      assert.throws(() => map.getOrInsert('x', 1), {
+        name: 'TypeError',
+      });
+      assert.throws(() => map.getOrInsertComputed('x', () => 1), {
+        name: 'TypeError',
+      });
+    });
+  } else {
+    // Node 26+
+    it('supports upsert', async () => {
+      const map = new Map<string, number>();
+      assert.equal(map.getOrInsert('x', 123), 123);
+      assert.equal(map.getOrInsert('x', 456), 123);
+      assert.equal(
+        map.getOrInsertComputed('x', () => 456),
+        123,
+      );
+      assert.equal(
+        map.getOrInsertComputed('y', () => 789),
+        789,
+      );
+    });
+  }
+
+  // https://github.com/tc39/proposal-temporal
+  if (process.version < 'v26') {
+    // Node 24
+    it('does not support Temporal yet', () => {
+      // compiles, but unfortunately, Node.js does not support yet
+      assert.throws(() => Temporal.Now.instant(), {
+        name: 'ReferenceError',
+        message: 'Temporal is not defined',
+      });
+    });
+  } else {
+    // Node 26+
+    it('supports Temporal', () => {
+      assert.equal(
+        Temporal.ZonedDateTime.from('2026-05-05T12:00:00[America/New_York]')
+          .toInstant()
+          .toString(),
+        '2026-05-05T16:00:00Z',
+      );
+    });
+  }
 });
